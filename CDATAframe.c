d215dcf1aeed8f04439e83f091a79609a66e08ce
Valentin <valent1.auffray@gmail.com>
2024-03-29 13:08:03 +0100

    def struct and create columns

--- CDATAframe.c
+++ CDATAframe.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "CDATAframe.h"
 
-int functions_exemple() {
-    // Your code here
+//4.1.1 columns
+#define REALOC_SIZE 256
 
-    return 0;
+int length(char* str){
+    int i = 0;
+    while (str[i] != '\0'){
+        i++;
+    }
+    return i;
 }
+
+COLUMN create_column(char* title){
+    COLUMN c;
+    for (int i = 0; i < length(title); i++){
+        c.title[i] = title[i];
+    }
+    return c;
+}
+
+//4.1.2 inserting a value in a column
+
--- CDATAframe.h
+++ CDATAframe.h
@@ -1,8 +1,17 @@
 #ifndef CDATAFRAME_H
 #define CDATAFRAME_H
+#include "CDATAframe.c"
+#define REALOC_SIZE 256
 
-// Include any necessary headers here
+typedef struct {
+    char* title;
+    int lsize;
+    int Psize;
+    int data;
+} COLUMN;
 
-// Declare your class or functions here
+int length(char* str);
+
+COLUMN create_column(char* title);
 
 #endif // CDATAFRAME_H
