2d9c7dbcf5e70e5cc3afc51bcca2784030c03479
Valentin <valent1.auffray@gmail.com>
2024-05-10 12:20:12 +0200

    beginnig of the main

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 //only way we found to compile C files on Windows PCs
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 
+    printf("\t\tWelcome to the CDATAFrame\n\tBy Oscar Masduppuy & Auffray Valentin\n\n");
+    
     char *titles[] = {"Big", "Column", "Bebou"};
     int num_columns = sizeof(titles) / sizeof(titles[0]);
 
